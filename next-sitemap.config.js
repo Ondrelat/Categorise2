d16de4e2675f92516fc08ac88d9275ecc52d8e60
Ondrelat <avrillon.benoit@orange.fr>
2025-02-16 12:45:20 +0100

    corrrection du sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.dicteeinteractive.fr',
+  siteUrl: process.env.SITE_URL || 'https://categorise.vercel.app/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://www.dicteeinteractive.fr/server-sitemap.xml',
+      'https://categorise.vercel.app/server-sitemap.xml',
     ],
   },
   outDir: './public', // Assurez-vous que c'est le bon dossier de sortie
