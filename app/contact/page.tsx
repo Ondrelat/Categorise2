26d525f9a9f2a55a69d31c734433fbad84de57aa
Ondrelat <avrillon.benoit@orange.fr>
2025-02-17 09:27:54 +0100

    enlever discord

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,20 +11,6 @@ export default async function Home() {
         <p className="text">Pour me contacter, envoyez-moi un mail :</p>
         <p className="email">avrillon.benoit73@gmail.com</p>
       </div>
-      <div className="mt-4 flex justify-center">
-        Tu peux aussi me rejoindre sur Discord.
-        <Link href="https://discord.gg/HWfnzXPeZU" target="_blank" rel="noopener noreferrer">
-          <div className="relative w-[30px] h-[30px]">
-            <Image
-              src="/discord-logo_3.png"
-              alt="Logo Discord"
-              layout="fill"
-              objectFit="contain"
-              className="bg-gray-900"
-            />
-          </div>
-        </Link>
-      </div>
     </div>
   );
 }
