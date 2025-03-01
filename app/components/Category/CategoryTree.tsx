327b8245164d9ee7cbd3e6219f38d1b9f0e97a00
Ondrelat <avrillon.benoit@orange.fr>
2025-03-01 10:10:49 +0100

    correction pour vercel

--- app/components/Category/CategoryTree.tsx
+++ app/components/Category/CategoryTree.tsx
@@ -4,13 +4,13 @@ import CategoryActions from './CategoryActions';
 import { useParams } from 'next/navigation';
 
 interface CategoryTreeProps {
-    initialCategoryName?: string; // name initial optionnel
+    initialName?: string; // name initial optionnel
 }
 
-export default async function CategoryTree({ initialCategoryName }: CategoryTreeProps) {
+export default async function CategoryTree({ initialName }: CategoryTreeProps) {
 
     // Récupérer la catégorie si un name est spécifié, sinon null
-    const category = initialCategoryName ? await getCategoryByName(initialCategoryName) : null;
+    const category = initialName ? await getCategoryByName(initialName) : null;
     
     // Récupérer toutes les catégories
     const categories = await getCategories();
