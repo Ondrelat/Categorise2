b704707aa14c8c3c4c5031e6d605ba1eb2576196
Ondrelat <avrillon.benoit@orange.fr>
2025-03-02 12:21:17 +0100

    decode l'url pour les accents

--- app/categories/[name]/layout.tsx
+++ app/categories/[name]/layout.tsx
@@ -15,22 +15,25 @@ export default async function CategoryLayout({
   children: React.ReactNode, 
   params: { name: string } 
 }) {
+  // Décoder le nom de la catégorie pour restaurer les accents et espaces
+  const decodedCategoryName = decodeURIComponent(params.name);
 
-    return (
-      <div className="flex flex-1 h-full mt-4 justify-center">
-        <div className="relative">
-          <div className="absolute -translate-x-full">
-            {/* La SideBar ne sera pas re-rendue si la catégorie est mise en cache */}
-            <SideBar categoryName={params.name} />
-          </div>
-          <div className="w-[800px] ml-4">
-            <div className="container mx-auto px-4 py-8">
-              <h1 className="text-3xl font-bold mb-6">{params.name}</h1>
-              <NavigatorSection />
-              {params.name === "Film" ? <FilmPage /> : children}
-            </div>
+  return (
+    <div className="flex flex-1 h-full mt-4 justify-center">
+      <div className="relative">
+        <div className="absolute -translate-x-full">
+          {/* Passer le nom décodé à la SideBar */}
+          <SideBar categoryName={decodedCategoryName} />
+        </div>
+        <div className="w-[800px] ml-4">
+          <div className="container mx-auto px-4 py-8">
+            {/* Afficher le nom décodé dans le titre */}
+            <h1 className="text-3xl font-bold mb-6">{decodedCategoryName}</h1>
+            <NavigatorSection />
+            {decodedCategoryName === "Film" ? <FilmPage /> : children}
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
